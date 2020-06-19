8e8f1ecf5631931518df0c056bf826d2eab93f15
Patrick Shushereba <pshushereba@gmail.com>
2020-06-18 21:01:12 -0700

    Started working with canvas.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   // Set the default size of the grid.
-  const [size, setSize] = useState(50);
+  const [size, setSize] = useState(20);
+  // Hold the initial empty grid in state.
+  const [grid, setGrid] = useState([]);
+  // Track whether the game is running or not.
+  const [running, setRunning] = useState(false);
+
+  console.log(grid);
+  useEffect(() => {
+    createGrid(size);
+  }, [size]);
 
   const createGrid = (num) => {
     const newGrid = new Array(num).fill(null);
     newGrid.forEach((item, idx) => {
       newGrid[idx] = new Array(num).fill(null);
     });
+    setGrid(newGrid);
     return newGrid;
   };
 
@@ -17,6 +27,15 @@ function App() {
     <div className="App">
       <h1>Conway's Game of Life</h1>
       <h2>Generation:</h2>
+      <div className="grid">
+        <canvas
+          style={{
+            height: `500px`,
+            width: `500px`,
+            backgroundColor: "#00b5ff",
+          }}
+        ></canvas>
+      </div>
       <button>Play</button>
       <button>Pause</button>
       <button>Stop</button>
@@ -25,3 +44,17 @@ function App() {
 }
 
 export default App;
+
+// {grid.map((item) => {
+//   return item.map((node) => {
+//     return (
+//       <canvas
+//         style={{
+//           height: `${500 / size}px`,
+//           width: `${500 / size}px`,
+//           backgroundColor: "#00b5ff",
+//         }}
+//       ></canvas>
+//     );
+//   });
+// })}
